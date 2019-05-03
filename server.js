f097e40662a48fc4fd530e43a7eda8e82095f192
SemperChen <semperchen@163.com>
2019-05-03 13:40:58 +0800

    spjx

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ let bodyParser = require('body-parser');
 let connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
-    password : '123456',
+    password : 'Csp080147201',
     port: '3306',
     database: 'test',
 });
@@ -22,11 +22,7 @@ app.post('/login', function (req, res) {
     login(params,res)
 
 })
-app.get('/login', function (req, res) {
-    let params = url.parse(req.url, true).query;
-    login(params,res)
 
-})
 function login(params,res) {
     let  sql = 'SELECT * FROM users WHERE username="'+params.username+'"';
 //查
@@ -48,11 +44,7 @@ app.post('/register', function (req, res) {
     register(req.body,res)
 
 })
-app.get('/register', function (req, res) {
-    let params = url.parse(req.url, true).query;
-    register(params,res)
 
-})
 function register(params,res) {
     let  sql = 'SELECT * FROM users WHERE username="'+params.username+'"';
 //查
@@ -63,8 +55,8 @@ function register(params,res) {
         }
         // console.log('result',result,'err',err,);
         if(result.length===0){
-            let  addSql = 'INSERT INTO users(username,password,bookmark) VALUES(?,?,?)';
-            let  addSqlParams = [params.username, params.password, params.bookmark];
+            let  addSql = 'INSERT INTO users(username,password,collection) VALUES(?,?,?)';
+            let  addSqlParams = [params.username, params.password, params.collection];
 //增
             connection.query(addSql,addSqlParams,function (err, result) {
                 if(err){
@@ -74,12 +66,12 @@ function register(params,res) {
                 login(params,res)
             });
         }else {
-            res.send({isRegisterSuccess:false,msc:'注册失败，用户名已存在！'});
+            res.send({isLogin:false,msc:'注册失败，用户名已存在！'});
         }
 
     });
 }
-function saveBookmark(params,res) {
+function saveCollection(params,res) {
     let  sql = 'SELECT * FROM users WHERE username="'+params.username+'"';
 //查
     connection.query(sql,function (err, result) {
@@ -89,29 +81,43 @@ function saveBookmark(params,res) {
         }
         // console.log('result',result[0],'err',err,);
         if(result.length===1&&result[0].username===params.username&&result[0].password===params.password){
-            let modSql = 'UPDATE users SET bookmark = ? WHERE username = ?';
-            let modSqlParams = [params.bookmark,params.username];
+            let modSql = 'UPDATE users SET collection = ? WHERE username = ?';
+            let modSqlParams = [params.collection,params.username];
 //改
             connection.query(modSql,modSqlParams,function (err, result) {
                 if(err){
                     // console.log('[UPDATE ERROR] - ',err.message);
                     // return;
                 }
-                res.send({isSavedBookmark:true,isLogin:true,msc:'已同步到云书架！',user:params});
+                res.send({isSavedCollection:true,isLogin:true,msc:'已同步！',user:params});
             });
         }else {
-            res.send({isSavedBookmark:true,msc:'保存失败！'});
+            res.send({isSavedCollection:true,msc:'保存失败！'});
         }
 
     });
 }
 app.post('/save', function (req, res) {
-    saveBookmark(req.body,res)
+    saveCollection(req.body,res)
 
 })
-app.get('/save', function (req, res) {
-    let params = url.parse(req.url, true).query;
-    saveBookmark(params,res)
+
+app.get('/data', function (req, res) {
+    let  sql = 'SELECT * FROM videos';
+//查
+    connection.query(sql,function (err, result) {
+        if(err){
+            // console.log('[SELECT ERROR] - ',err.message);
+            // return;
+        }
+        // console.log('result',result,'err',err,);
+        if(result.length>0){
+            res.send(result);
+        }else {
+            res.send([]);
+        }
+
+    });
 
 })
 
