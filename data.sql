3d465b4c8cb738f2a1a213f6781a8d6bfdaecc8f
Semper <semper@SemperdeMacBook-Air.local>
2021-01-06 15:13:32 +0800

    视频

--- data.sql
+++ data.sql
@@ -1,22 +1,8 @@
 create table users(
-    id INT(5) PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(18),
-    password VARCHAR(50),
+    id INT(10) PRIMARY KEY AUTO_INCREMENT,
+    pid VARCHAR(100) UNIQUE NOT NULL,
+    platform VARCHAR(20),
+    count1 INT(10),
+    count2 INT(10)
+);
 
-    school VARCHAR(200),
-    email VARCHAR(200),
-    phone VARCHAR(200),
-    address VARCHAR(200)
-);
-create table videos(
-    id INT(5) PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(50),
-    author VARCHAR(50),
-    img VARCHAR(200),
-    intro TEXT,
-    viewersCount INT(10),
-    score FLOAT(3,1),
-    video VARCHAR(200),
-    pdf VARCHAR(200)
-);
-INSERT INTO videos(title,author,img,intro,viewersCount,score,video,pdf) VALUES('PHP7进阶到架构实战6','edgar a. guest','JavaScript/1/1.png','本套AE入门视频教程特邀请了经验丰富的胡老师为大家授课，希望大家从中体验AE的魅力，掌握这一前沿的技术。After Effects简称AE,是Adobe公司推出的一款图形视频处理软件，适用于从事设计和视频特技的机构，包括电视台、动画制作公司、个人后期制作工作室以及多媒体工作室。目前是十分流行火爆的后期制作软件，许多电影特效，电视广告、片头动画都出自AE合成。',200,4.8,'JavaScript/1/1.mp4','JavaScript/1/1.pdf');
--- server.js
+++ server.js
@@ -16,28 +16,30 @@ let connection = mysql.createConnection({
 connection.connect();
 
 app.use(bodyParser.json()); // for parsing application/json
-// app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')))
 
-app.post('/login', function (req, res) {
-    let params = req.body;
+app.get('/login', function (req, res) {
+    let params = req.query;
+    console.log("login params:",params,params.pageIndex,params.pageSize)
     login(params, res)
 
 })
 
 function login(params, res) {
-    let sql = 'SELECT * FROM users WHERE username="' + params.username + '"';
+    let  pageIndex = parseInt(params.pageIndex)
+    let  pageSize = parseInt(params.pageSize)
+
+    let sql = 'SELECT * FROM users limit '+(pageIndex-1)*pageSize+','+pageSize;
 //查
     connection.query(sql, function (err, result) {
         if (err) {
-            // console.log('[SELECT ERROR] - ',err.message);
-            // return;
+            console.log('[SELECT ERROR] - ',err.message);
+            res.send({ok: false, msc: '查询失败！'+err.message});
+            return;
         }
-        // console.log('result',result,'err',err,);
-        if (result.length === 1 && result[0].username === params.username && result[0].password === params.password) {
-            res.send({isLogin: true, msc: '登录成功！', user: result[0]});
-        } else {
-            res.send({isLogin: false, msc: '登录失败！用户名或密码错误'});
+        if(result.length>0){
+            res.send({ok: true, msc: '登录成功！', user: result});
         }
 
     });
@@ -49,116 +51,18 @@ app.post('/register', function (req, res) {
 })
 
 function register(params, res) {
-    let sql = 'SELECT * FROM users WHERE username="' + params.username + '"';
-//查
-    connection.query(sql, function (err, result) {
+    let addSql = 'INSERT INTO users(pid,platform,count1,count2) VALUES(?,?,?,?)';
+    let addSqlParams = [params.pid, params.platform, params.count1, params.count2];
+    //增
+    connection.query(addSql, addSqlParams, function (err, result) {
         if (err) {
-            // console.log('[SELECT ERROR] - ',err.message);
-            // return;
+            console.log('[INSERT ERROR] - ', err.message);
+            res.send({success: '插入失败！',ok:false,msc:err.message});
+            return;
         }
-        // console.log('result',result,'err',err,);
-        if (result.length === 0) {
-            let addSql = 'INSERT INTO users(username,password) VALUES(?,?)';
-            let addSqlParams = [params.username, params.password];
-//增
-            connection.query(addSql, addSqlParams, function (err, result) {
-                if (err) {
-                    console.log('[INSERT ERROR] - ', err.message);
-                    return;
-                }
-                login(params, res)
-            });
-        } else {
-            res.send({isLogin: false, msc: '注册失败，用户名已存在！'});
-        }
-
+        res.send({success: '插入成功！',ok:true});
     });
 }
-
-function saveUserInfo(params, res) {
-    let sql = 'SELECT * FROM users WHERE username="' + params.username + '"';
-    connection.query(sql, function (err, result) {
-        if (err) {
-            // console.log('[SELECT ERROR] - ',err.message);
-            // return;
-        }
-        // console.log('result',result[0],'err',err,);
-        if (result.length === 1 && result[0].username === params.username && result[0].password === params.password) {
-            let modSql = 'UPDATE users SET school=?,email=?,phone=?,address=? WHERE username = ?';
-            let modSqlParams = [params.school, params.email, params.phone, params.address, params.username];
-//改
-            connection.query(modSql, modSqlParams, function (err, result) {
-                if (err) {
-                    // console.log('[UPDATE ERROR] - ',err.message);
-                    // return;
-                }
-                console.log('user', params)
-                res.send({isSaved: true, isLogin: true, msc: '已同步！', user: params});
-            });
-        } else {
-            res.send({isSaved: false, msc: '保存失败！'});
-        }
-
-    });
-}
-
-app.post('/save', function (req, res) {
-    saveUserInfo(req.body, res)
-
-})
-
-function saveViewersCount(params, res) {
-    let sql = 'SELECT * FROM videos WHERE id="' + params.id + '"';
-    connection.query(sql, function (err, result) {
-        if (err) {
-            // console.log('[SELECT ERROR] - ',err.message);
-            // return;
-        }
-        // console.log('result',result[0],'err',err,);
-        if (result.length === 1 && result[0].id === params.id) {
-            let modSql = 'UPDATE videos SET viewersCount=? WHERE id = ?';
-            let count = result[0].viewersCount + 1;
-            let modSqlParams = [count, params.id];
-            console.log('modSqlParams',modSqlParams)
-            //改
-            connection.query(modSql, modSqlParams, function (err, result) {
-                if (err) {
-                    // console.log('[UPDATE ERROR] - ',err.message);
-                    // return;
-                }
-                res.send({isSaved: true, msc: '保存成功！'});
-            });
-        } else {
-            res.send({isSaved: false, msc: '保存失败！'});
-        }
-
-    });
-}
-
-app.post('/saveViewersCount', function (req, res) {
-    saveViewersCount(req.body, res)
-
-})
-
-app.get('/data', function (req, res) {
-    let sql = 'SELECT * FROM videos ORDER BY viewersCount DESC';
-//查
-    connection.query(sql, function (err, result) {
-        if (err) {
-            // console.log('[SELECT ERROR] - ',err.message);
-            // return;
-        }
-        // console.log('result',result,'err',err,);
-        if (result.length > 0) {
-            res.send(result);
-        } else {
-            res.send([]);
-        }
-
-    });
-
-})
-
 
 // connection.end();
 let server = app.listen(8080, function () {
